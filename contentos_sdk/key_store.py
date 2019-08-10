d2b6982f7b28aafaf8317e453c5850af5129d08f
brickgao <brickgao@gmail.com>
2019-08-10 17:27:02 +0800

    implemented the class related to key store

---
+++ contentos_sdk/key_store.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import json
+
+from Crypto.Hash import SHA256
+
+from contentos_sdk.cipher import AESCipher
+
+
+class KeyStore(object):
+
+    def __init__(self):
+        self.account_to_key = dict()
+        self.aes_cipher = AESCipher()
+
+    def get_key(self, account):
+        return self.account_to_key[account]
+
+    def add_key(self, account, key):
+        self.account_to_key[account] = key
+
+    def remove_key(self, account):
+        self.account_to_key.pop(account)
+
+    def get_accounts(self):
+        return self.account_to_key.keys()
+
+    def load_from_file(self, filename, password):
+        secret_key = SHA256.new().update(password).digest()
+        with open(filename, "rb") as f:
+            buf = f.read()
+        json_data = self.aes_cipher.decrypt(secret_key, buf)
+        self.self.account_to_key = json.loads(json_data)
+
+    def dump_to_file(self, filename, password):
+        secret_key = SHA256.new().update(password).digest()
+        json_data = json.dumps(self.account_to_key)
+        buf = self.aes_cipher.encrypt(secret_key, json_data)
+        with open(filename, "wb") as f:
+            f.write(buf)
