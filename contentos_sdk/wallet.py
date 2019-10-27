266a8c61c8b1cc5f8e013636e22a3e7c6456eb78
brickgao <brickgao@gmail.com>
2019-10-27 19:44:24 +0800

    supported chain name and chain ID

--- contentos_sdk/wallet.py
+++ contentos_sdk/wallet.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import zlib
+
 import grpc
 
 from contentos_sdk.account import Account
-from contentos_sdk.grpc_pb2 import grpc_pb2
+from contentos_sdk.grpc_pb2 import grpc_pb2_grpc
 
 
 class Wallet(object):
 
-    def __init__(self, ip, port):
+    def __init__(self, chain_name, ip, port):
+        self.chain_name = chain_name
+        self.chain_id = zlib.crc32(chain_name.encode("ascii"))
         self.ip, self.port = ip, port
-        self.channel = grpc.insecure_channel(ip + port)
-        self.stub = grpc_pb2.ApiService(self.channel)
+        self.channel = grpc.insecure_channel(ip + ":" + str(port))
+        self.stub = grpc_pb2_grpc.ApiServiceStub(self.channel)
 
     def get_account(self, key):
-        return Account(self.stub, key)
+        return Account(self.stub, self.chain_name, self.chain_id, key)
