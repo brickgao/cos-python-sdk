5ef101ed1242cef6589565790a9eb112952e1ecd
brickgao <brickgao@gmail.com>
2019-11-10 00:38:35 +0800

    removed __all__ in __init__.py

--- contentos_sdk/__init__.py
+++ contentos_sdk/__init__.py
@@ -2,17 +2,3 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.0.1"
-
-from contentos_sdk import operation
-from contentos_sdk.account import Account
-from contentos_sdk.cipher import Secp256k1Cipher
-from contentos_sdk.transcation import Transaction
-from contentos_sdk.wallet import Wallet
-
-__all__ = [
-    "Account",
-    "Wallet",
-    "Secp256k1Cipher",
-    "Transaction",
-    "operation"
-]
